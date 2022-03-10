8d250293c2d117ec2128b26924bc630dfb803ac0
BoglarkaToth <tothboglarka@gmail.com>
2022-03-10 16:08:42 +0100

    boolean

---
+++ boolean_exprassions.py
@@ -0,0 +1,27 @@
+print(True)
+print(False)
+
+print(4 < 10)
+print(4 > 10)
+
+print(10 > 10)
+print(10 >= 10)
+
+print(10 == 10)
+i = 5
+print(i == 5)
+
+print("alma" == "alma")
+print("alma" == "körte")
+
+print("alma" < "körte")
+print("ábel" > "cecil")
+
+print(True or False)
+print(True and False)
+
+print(2 > 3 or 8 < 10)
+print(2 > 3 and 8 < 10)
+
+print(not 3 > 2)
+print(not "alma" == "korte")
