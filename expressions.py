fd80907bbe284be8208867e14aa4d47cf97cbbcd
BoglarkaToth <tothboglarka@gmail.com>
2022-03-10 14:27:01 +0100

    kifejezések

---
+++ expressions.py
@@ -0,0 +1,20 @@
+print(3 ++ 2)
+print(10 // 3)
+print(10 % 3)
+
+print(int(4.3))
+print(int(4.9))
+
+i = 7 + 3
+print(i)
+
+# precedencia táblázat...
+
+i = 7 + 3 * 2
+i = (7 + 3) * 2
+i = 2
+j = 4
+i = ((7 + i * j) * len("alma"))
+print(i)
+
+print("x" * 3)
