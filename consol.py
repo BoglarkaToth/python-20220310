d3c5e4a33026141b36dc1f285c1e6701798cb0b0
BoglarkaToth <tothboglarka@gmail.com>
2022-03-10 14:41:12 +0100

    inputs

---
+++ consol.py
@@ -0,0 +1,18 @@
+name = input("Mi a neved?")
+print(name)
+
+print(type(name))
+
+age = input("Hány éves vagy?")
+print(type(age))
+age_double = age * 2
+age_double_2 = int(age) * 2
+
+print(age)
+print(age_double)
+print(age_double_2)
+print(int(age) * 2)
+
+# age = int(input(("Hány éves?")))
+# print(age * 2)
+
