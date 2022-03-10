ac0a34867fd970f57899b12738d4ec922c1570a3
BoglarkaToth <tothboglarka@gmail.com>
2022-03-10 16:40:01 +0100

    find button and click

---
+++ hello-selenium.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# open browser
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://python.org")
+
+# find go button
+driver.find_element(By.ID, "id-search-field").send_keys("precedence")
+
+# click button
+driver.find_element(By.ID, "submit").click()
+
+print("end")
